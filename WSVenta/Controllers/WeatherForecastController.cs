4255f2bcda89a4b5f6ec7bbf9549651dfa58b8b9
Edison <edisonalberto21@.hotmail.com>
2020-11-21 03:25:12 +0000

    Add project files.

---
+++ WSVenta/Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WSVenta.Models;
+
+namespace WSVenta.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (VentaRealContext db = new VentaRealContext())
+            {
+                var lst = db.Clientes.ToList();
+                return Ok(lst);
+            }
+        }
+    }
+}
